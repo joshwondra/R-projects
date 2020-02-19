313aeb6279a0e09a94b5258aafc237a38898a33f
joshwondra <jdwondra@umich.edu>
2020-02-19 09:34:27 -0500

    Revision pre-discussion
    Completed a full revision of all sections prior to the Discussion,
    including some improved notation
    
    Also noted that there’s a mistake in the Welch degrees of freedom that
    must be corrected in the equations and simulations, but the impact on
    the final results should be minor (it only affects the simple effects)

--- Welch rule-ANOVA/Welch functions.R
+++ Welch rule-ANOVA/Welch functions.R
@@ -9,16 +9,6 @@ legend_name <- expression(paste(
   n[A[2]*B[2]]
   ))
   
-  # paste0('Sample sizes\nGroup ', expression(a[1]b[1]), 
-  # ', ',
-  # expression(a[1]b[2]), 
-  # ', ', 
-  # expression(a[2]b[1]), 
-  # ', ', 
-  # expression(a[2]b[2])
-  # )
-  # 
-  # 'Sample sizes \nGroup A[1]*B[1], A[1]*B[2], A[2]*B[1], A[2]*B[2]'
 
 # Functions for Rnw - Welch Manuscript - ANOVA
 
